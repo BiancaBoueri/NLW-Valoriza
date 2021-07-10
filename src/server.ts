4e02716de3725edd74e2653914623be8174549c4
Bianca Boueri <biancaglyciaboueri@gmail.com>
2021-07-09 22:41:28 -0300

    Paulenriks

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 
-app.listen(3000, () => console.log("Server is running aaaaaaaaa"))
+app.listen(3000, () => console.log("Server is running aaaaaaaaaAAAAAAAAAAAA"))
 
 
 app.get("/test", (request, response) => {
