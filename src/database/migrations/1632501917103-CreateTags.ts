21b3409712270b72c22ffdbc822e21096b71bd4f
Bianca Boueri <biancaglyciaboueri@gmail.com>
2021-10-06 14:30:27 -0300

    JWT

--- src/database/migrations/1632501917103-CreateTags.ts
+++ src/database/migrations/1632501917103-CreateTags.ts
@@ -36,33 +36,3 @@ export class CreateTags1632501917103 implements MigrationInterface {
     }
 
 }
-
-// aula 3 => criando a entidade TAG:::::::::::::::::::::::
-
-/* CRIANDO TAG (uma nova entidade) ::::::::::::::
-
-1. MIGRATION:
-
-yarn typeorm migration:create -n CreateTags
-escrever o arquivo da migration.
-dps rodar: yarn typeorm migration:run
-ver se apareceu no beekeeper
-
-dps...
-
-(criar normalmente os arquivos)
-
-2. ENTIDADE
-3. REPOSITORIO
-4. SERVICES (as regras,logica)
-5. CONTROLLER
-
-- referenciar em routes.ts
-
-- yarn dev
-
-- no insomnia => criar new request: Create Tag (POST) (JSON) (_.baseURL/tags), escrever o corpo tbm, SEND
-
-agora tag ta funcionando. 40:40
-
-*/
--- src/routes.ts
+++ src/routes.ts
@@ -2,17 +2,18 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middleware/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
-
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/tags", ensureAdmin/*middleware*/, createTagController.handle);
 router.post("/users", createUserController.handle);
-
+router.post("/login", authenticateUserController.handle);
 
 export { router };
 
